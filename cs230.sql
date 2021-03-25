55799b88072032763978f1fc5ddec8dc78625f7f
Joseph Shepard <jss0045@mix.wvu.edu>
2021-03-25 12:32:22 -0700

    Update cs230.sql
    fix sql

--- cs230.sql
+++ cs230.sql
@@ -31,7 +31,7 @@ CREATE TABLE `gallery` (
   `title` varchar(60) NOT NULL,
   `descript` text NOT NULL,
   `picpath` varchar(80) NOT NULL,
-  `upload_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `upload_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `rating` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
 
